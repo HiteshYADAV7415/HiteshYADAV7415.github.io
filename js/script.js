2c02c0d0a2c96fa0cd7a7bf409f42240f2bbb052
Hiteshwar Sahai <nikyad2019@gmail.com>
2023-03-14 20:57:52 +0530

    final

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
-
+document.getElementById("resume_button").addEventListener("click",()=>{
+    window.open('https://drive.google.com/file/d/1Jwl6ruCG9ckPPp5sIE0yBFV7UeFuAO55/view?usp=sharing', '_blank');
+})
