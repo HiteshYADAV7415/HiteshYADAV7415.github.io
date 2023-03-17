0fc1e096cc5c01c7acfc6ccd66e26e04c15e1225
Hiteshwar Sahai <nikyad2019@gmail.com>
2023-03-17 21:42:43 +0530

    soft,customer skill and resume button has been updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 document.getElementById("resume_button").addEventListener("click",()=>{
     window.open('https://drive.google.com/file/d/1Jwl6ruCG9ckPPp5sIE0yBFV7UeFuAO55/view?usp=sharing', '_blank');
 })
+
+document.getElementById("abt_rbtn").addEventListener("click",()=>{
+    window.open('https://drive.google.com/file/d/1Jwl6ruCG9ckPPp5sIE0yBFV7UeFuAO55/view?usp=sharing', '_blank');
+})
+
+document.getElementById("linkedinbtn").addEventListener("click",()=>{
+    window.open('https://www.linkedin.com/in/hiteshwar-sahai-923840154/', '_blank');
+})
+
+document.getElementById("githubbtn").addEventListener("click",()=>{
+    window.open('https://github.com/HiteshYADAV7415', '_blank');
+})
