c478d58b63cc57e3ae2dec84a2bb245b50eb297b
Hiteshwar Sahai <nikyad2019@gmail.com>
2023-03-18 12:30:43 +0530

    Project Section

--- js/script.js
+++ js/script.js
@@ -13,3 +13,25 @@ document.getElementById("linkedinbtn").addEventListener("click",()=>{
 document.getElementById("githubbtn").addEventListener("click",()=>{
     window.open('https://github.com/HiteshYADAV7415', '_blank');
 })
+document.getElementById("fgbtn").addEventListener("click",()=>{
+    window.open('https://github.com/HiteshYADAV7415/Figma-User-Onboarding-', '_blank');
+})
+document.getElementById("fdbtn").addEventListener("click",()=>{
+    window.open('https://melodic-figolla-a9606b.netlify.app/', '_blank');
+})
+document.getElementById("mplgbtn").addEventListener("click",()=>{
+    window.open('https://github.com/HiteshYADAV7415/Masai-Premier-League', '_blank');
+})
+document.getElementById("mpldbtn").addEventListener("click",()=>{
+    window.open('https://shiny-griffin-ab0879.netlify.app/', '_blank');
+})
+document.getElementById("merdbtn").addEventListener("click",()=>{
+    window.open('https://verdant-churros-2a2e8f.netlify.app/', '_blank');
+})
+document.getElementById("mergbtn").addEventListener("click",()=>{
+    window.open('https://github.com/HiteshYADAV7415/Masai-Evaluation-Report', '_blank');
+})
+
+
+
+
